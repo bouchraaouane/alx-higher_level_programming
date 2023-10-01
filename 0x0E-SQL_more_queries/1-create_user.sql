bd9f8bbada5c270386fec7cd80200e2f2d7b444b
bouchraaouane <bouchraaouane1@gmail.com>
2023-10-01 07:33:13 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
--- creates a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creates a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grants SELECT privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
